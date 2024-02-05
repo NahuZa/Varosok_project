3e6cf6c6cd45fd54b248d2ce6fd22e60d15e7425
NahuZa <152151052+NahuZa@users.noreply.github.com>
2024-02-05 11:04:24 +0100

    alap html oldal
    annyi hogy az index_html.php fájlban a header háttere úgy van meghívva a style.css fájlban hogy egy pics nevű mappában kell lennie

---
+++ index_html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Városok</title>
+</head>
+<body class="hatter">
+    <header>
+        <div class="cim">
+            <h1>Megyék és Városaik</h1>
+        </div>
+    </header>
+
+    <form>
+        <h2>Válassza ki a megyét:</h2> 
+            <button>Nógrád</button><!--Példa-->
+            <button>Pest</button><!--Példa-->
+            <button>Borsod</button><!--Példa-->
+            <button>Somogy</button><!--Példa-->
+            <!-- összes megye mint egy gomb szépen sorban? => táblából kiirás => kattintásra azoknak a települáseit sorolja be egymás alá => cars/makers-->
+    </form>
+</body>
+</html>
