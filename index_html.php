d7ded6ef569e88c201b5ae4902e95e7601abc378
3041TAN-17 <3041TAN-17@boronkay.local>
2024-02-12 09:02:11 +0100

    asd

---
+++ index_html.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Városok</title>
+</head>
+<body class="hatter">
+    <header>
+        <div class="cim">
+            <h1>Megyék és Városaik</h1>
+        </div>
+    </header>
+
+    <h2>Válassza ki a megyét:</h2> 
+
+    <?php
+
+    require_once('osztaly.php');
+
+   $tabla=new osztaly();
+
+   $megyek=$tabla->counties();
+
+   
+   foreach ($megyek as $megye)
+   {
+    echo '<form method="post" action="">'
+    .'<tr>'
+        .'<td> <button class="button" value='.$megye['countyId'].'name="megye-button">'.$megye['county'].'</button> </td>'
+        . '<td><div style="display: flex">'
+        . '</div></td>'
+    . '</tr></form>';
+   }
+   
+
+
+    ?>
+</body>
+</html>
---
+++ osztaly.php
@@ -0,0 +1,27 @@
+<?php
+
+class Osztaly{
+    protected $mysqli;
+        function __construct($host='localhost', $user='root', $password='', $db='csv_db 14')
+        {
+            $this->mysqli=new mysqli($host, $user, $password, $db);
+            if ($this->mysqli->connect_errno)
+            {
+                throw new Exception($this->mysqli->connect_errno);
+            }
+        }
+
+        function __destruct()
+        {
+            $this->mysqli->close();
+        }
+
+        public function counties(): array
+        {
+        $query="SELECT * FROM counties";
+
+        return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+}
+
+?>
